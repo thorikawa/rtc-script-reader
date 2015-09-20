33b54af1b67105b1b83d73404985dc8a2138e0b6
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2015-09-21 06:02:00 +0900

    Initial commit

---
+++ idlcompile.bat
@@ -0,0 +1,10 @@
+echo off
+setlocal
+for %%I in (python.exe) do if exist %%~$path:I set f=%%~$path:I
+if exist %f% do (
+  %f:python.exe=%omniidl.exe -bpython -I"%RTM_ROOT%rtm\idl" -I"/usr/local/include/openrtm-1.1/rtm/idl" idl/ExtendedDataTypes.idl idl/ExtendedDataTypes.idl 
+) else do (
+  echo "python.exe" can not be found.
+  echo Please modify PATH environmental variable for python command.
+)
+endlocal
---
+++ idlcompile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+omniidl -bpython -I"%RTM_ROOT%rtm\idl" -I"/usr/local/include/openrtm-1.1/rtm/idl" idl/ExtendedDataTypes.idl idl/ExtendedDataTypes.idl 
