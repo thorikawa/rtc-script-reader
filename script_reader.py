a2aaafba31917a2b7301287347a95c98326ec926
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2015-09-24 09:00:46 +0900

    Add noisy part detec logic

--- script_reader.py
+++ script_reader.py
@@ -17,6 +17,13 @@ sys.path.append(".")
 import RTC
 import OpenRTM_aist
 
+import json
+import requests
+import pyaudio
+import shutil
+import wave
+import numpy as np
+import math
 
 # Import Service implementation class
 # <rtc-template block="service_impl">
@@ -31,15 +38,15 @@ import OpenRTM_aist
 # This module's spesification
 # <rtc-template block="module_spec">
 script_reader_spec = ["implementation_id", "script_reader", 
-		 "type_name",         "script_reader", 
-		 "description",       "ModuleDescription", 
-		 "version",           "1.0.0", 
-		 "vendor",            "Takahiro Horikawa", 
-		 "category",          "Development", 
-		 "activity_type",     "STATIC", 
-		 "max_instance",      "1", 
-		 "language",          "Python", 
-		 "lang_type",         "SCRIPT",
+		 "type_name",		 "script_reader", 
+		 "description",	   "ModuleDescription", 
+		 "version",		   "1.0.0", 
+		 "vendor",			"Takahiro Horikawa", 
+		 "category",		  "Development", 
+		 "activity_type",	 "STATIC", 
+		 "max_instance",	  "1", 
+		 "language",		  "Python", 
+		 "lang_type",		 "SCRIPT",
 		 "conf.default.script_file", "/tmp/script.txt",
 		 "conf.__widget__.script_file", "text",
 		 ""]
@@ -133,9 +140,15 @@ class script_reader(OpenRTM_aist.DataFlowComponentBase):
 	#	# @return RTC::ReturnCode_t
 	#	#
 	#	#
-	#def onStartup(self, ec_id):
-	#
-	#	return RTC.RTC_OK
+	def onStartup(self, ec_id):
+		print "load script file"
+		f = open(self._script_file[0])
+		self.script_data = json.load(f)
+		print str(self.script_data)
+
+		self.audio = pyaudio.PyAudio()
+		self.onActivated(1)
+		return RTC.RTC_OK
 	
 	#	##
 	#	#
@@ -161,10 +174,89 @@ class script_reader(OpenRTM_aist.DataFlowComponentBase):
 	#	# @return RTC::ReturnCode_t
 	#	#
 	#	#
-	#def onActivated(self, ec_id):
-	#
-	#	return RTC.RTC_OK
+	def onActivated(self, ec_id):
+		print "onActivated"
+		for line in self.script_data:
+			self.speak(line["audio_url"])
+			if line["robot_id"] == 1:
+				pass
+			elif line["robot_id"] == 2:
+				pass
+			else:
+				print "invalid robot id:" + str(line.robot_id)
+		return RTC.RTC_OK
 	
+	def speak(self, audio_url):
+		cache_file = "cache.wav"
+		res = requests.get(audio_url, stream=True)
+		with open(cache_file, "wb") as fp:
+			shutil.copyfileobj(res.raw, fp)
+
+		wf = wave.open(cache_file, "r")
+		sampleRate = wf.getframerate()
+		channels = wf.getnchannels()
+		stream = self.audio.open(
+			format = self.audio.get_format_from_width(wf.getsampwidth()),
+			channels = channels,
+			rate = sampleRate,
+			output = True)
+
+		amp  = (2**8) ** wf.getsampwidth() / 2
+		print "nframe=%d" % wf.getnframes()
+		buffer = wf.readframes(wf.getnframes())
+		data = np.frombuffer(buffer, dtype="int16")
+		# normalize
+		data = data / float(amp)
+		print "len=%d" % len(data)
+		# for i in range(len(data)/2):
+		# 	d = data[2*i] * 256 + data[2*i + 1]
+		# 	print str(d)
+		threshold = 0.05
+		minSilentDurationInSec = 0.2
+		minSilentSamples = minSilentDurationInSec * sampleRate
+
+		start = 0
+		index = 0
+		lowCount = 0
+		silentPart = []
+		noisyPart = []
+
+		for i in range(len(data)):
+			if math.fabs(data[i]) < threshold:
+				if lowCount == 0:
+					start = index
+				lowCount = lowCount + 1
+			else:
+				if lowCount > minSilentSamples:
+					silentPart.append(start)
+					silentPart.append(index)
+				lowCount = 0
+			index = index + 1
+
+		if lowCount > minSilentSamples:
+			silentPart.append(start)
+			silentPart.append(index - 1)
+
+		offset = 0
+		for i in range(len(silentPart) / 2):
+			if silentPart[2*i] > offset:
+				noisyPart.append(offset / float(sampleRate))
+				noisyPart.append(silentPart[2*i] / float(sampleRate))
+			offset = silentPart[2*i + 1]
+
+		if offset < index - 1:
+			noisyPart.append(offset / float(sampleRate))
+			noisyPart.append((index - 1) / float(sampleRate))
+
+		noisyPart = map(lambda t: 1000*t, noisyPart)
+		print str(noisyPart)
+
+		# stream.write(buffer)
+		# stream.close()
+		self.audio.terminate()
+
+		pass
+
 	#	##
 	#	#
 	#	# The deactivated action (Active state exit action)
@@ -271,16 +363,16 @@ class script_reader(OpenRTM_aist.DataFlowComponentBase):
 
 
 def script_readerInit(manager):
-    profile = OpenRTM_aist.Properties(defaults_str=script_reader_spec)
-    manager.registerFactory(profile,
-                            script_reader,
-                            OpenRTM_aist.Delete)
+	profile = OpenRTM_aist.Properties(defaults_str=script_reader_spec)
+	manager.registerFactory(profile,
+							script_reader,
+							OpenRTM_aist.Delete)
 
 def MyModuleInit(manager):
-    script_readerInit(manager)
+	script_readerInit(manager)
 
-    # Create a component
-    comp = manager.createComponent("script_reader")
+	# Create a component
+	comp = manager.createComponent("script_reader")
 
 def main():
 	mgr = OpenRTM_aist.Manager.init(sys.argv)
